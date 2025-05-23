526d587332f6307e1fc4ebdc2fa5951a94096571
caiyuehong <15146181+cyh0815@user.noreply.gitee.com>
2025-05-23 01:52:45 +0000

    update demo/calc/wap.c.
    Signed-off-by: caiyuehong <15146181+cyh0815@user.noreply.gitee.com>

--- demo/calc/wap.c
+++ demo/calc/wap.c
@@ -79,66 +79,35 @@ int32_t op_testzvwWap()
 int32_t op_testrvvWap()
 {
     size_t vl, avl;
-    uint32_t vtypeE;
+    uint32_t *pz = (uint32_t*)VM_RST2_RVV_ADDR;
+    uint32_t syncRd;
 
-    uint32_t num0 = 0;
-    uint32_t num16 = 16;
-    int32_t *a0Addr = aWapAddr[0];
-    int32_t *a1Addr = aWapAddr[1];    
-    vint32m1_t vA,vB;
-    vint32m1_t vReal,vImag,vRealShfit;
-    vint32m1_t vTempShift;
-    vint32m1_t vPowReal,vPowImag;
-    vint32m2_t vAcc,vAPow,vBPow;
-    vint32m2_t vAPowShif,vBPowShift,vShift,vPowSum,vPowSumShift,vSum;
-    uint32_t accShfit = 0, mulShfit = 0;                
-    
+    asm volatile("vsetvli %[vl], %[avl],208;":[vl] "=&r" (vl):[avl] "r" (32));
+    asm volatile("vsub.vv v0, v0,v0;"::);//VALU1
+    asm volatile("vle32.v v2, (%[a0Addr]);"::[a0Addr]"r"(VM_SRC1_ADDR));//LSD
+    asm volatile("vsrl.vx v3, v2, %[num16];"::[num16]"r"(16));//VALU2 IM(L)
+    asm volatile("vle32.v v1, (%[gainShiftAddr]);" ::[gainShiftAddr]"r"(VM_GAIN_RVV_ADDR));//LSD
+    asm volatile("vsll.vx v4, v2, %[num16];"::[num16]"r"(16));//VALU2 RE(H)
+    asm volatile("vle32.v v9, (%[a1Addr]);"::[a1Addr]"r"(VM_SRC1_ADDR + 32*4));//LSD
+    asm volatile("vsrl.vx v5, v4, %[num16];"::[num16]"r"(16));//VALU2 RE(L)
+    asm volatile("vsrl.vx v13, v9, %[num16];"::[num16]"r"(16));//VALU2 IM1(L)
+    asm volatile("vmul.vv v6, v5, v5;"::);//VALU1 RE(L)*RE(L)
+    asm volatile("vsll.vx v14, v9, %[num16];"::[num16]"r"(16));//VALU2 RE1(H)
+    asm volatile("vsra.vx v6, v6, %[mulShfit];"::[mulShfit]"r"(0));//VALU2
+    asm volatile("vmul.vv v7, v3, v3;"::);//VALU1 IM(L)*IM(L)
+    asm volatile("vsrl.vx v15, v14, %[num16];"::[num16]"r"(16));//VALU2 RE1(L)
+    asm volatile("vsra.vx v7, v7, %[mulShfit];"::[mulShfit]"r"(0));//VALU2
+    asm volatile("vmul.vv v17, v13, v13;"::);//VALU1 IM1(L)*IM1(L)
+    asm volatile("vadd.vv v8, v7, v6;"::[num16]"r"(16));//VALU1 RE(L)*RE(L)+IM(L)*IM(L)
+    asm volatile("vmul.vv v16, v15, v15;"::);//VALU1 RE1(L)*RE1(L)
+    asm volatile("vsra.vv v11, v8, v1;"::);//VALU2 vsra(RE(L)*RE(L)+IM(L)*IM(L))
+    asm volatile("vadd.vv v10, v17, v16;"::);//VALU1 RE1(L)*RE1(L)+IM1(L)*IM1(L)
+    asm volatile("vsra.vv v12, v10, v1;"::);//VALU2 vsra(RE1(L)*RE1(L)+IM1(L)*IM1(L))
+    asm volatile("vadd.vv v13, v11, v12;"::);//VALU1
+    asm volatile("vsra.vx v14, v13, %[accShfit];"::[accShfit]"r"(0));//VALU2
+    asm volatile("vredsum.vs v15, v14, v0;"::);//VALU2
+    asm volatile("vmv.x.s  %[result], v15;":[result]"=&r"(*pz):);//VALU2
+    asm volatile("vsync %[syncRd],%[rs2];":[syncRd]"=&r"(syncRd):[rs2]"r"(0xFF));
 
-    avl = 32;
-    vtypeE = TA | MA | M1 | E32;
-    asm volatile("vsetvl %[vl], %[avl], %[vtype]": [vl] "=r" (vl) : [avl] "r" (avl), [vtype] "r" (vtypeE));
-    asm volatile("vsub.vv %[vAcc], %[vAcc],%[vAcc];"
-                 :[vAcc]"=vr"(vAcc)
-                 :[num0]"r"(num0));    
-
-    asm volatile("vle32.v %[vShift], (%[gainShiftAddr]);" 
-                  :[vShift]"+vr"(vShift)
-                  :[gainShiftAddr]"r"(gainShiftAdrrRvv)); 
-                     
-    vtypeE = M1 | E32;
-    asm volatile("vsetvl %[vl], %[avl], %[vtype]": [vl] "=r" (vl) : [avl] "r" (avl), [vtype] "r" (vtypeE));              
-    asm volatile("vle32.v %[vA], (%[a0Addr]);\
-                  vsrl.vx %[vImag], %[vA], %[num16];\
-                  vsll.vx %[vRealShfit], %[vA], %[num16];\
-                  vsrl.vx %[vReal], %[vRealShfit], %[num16];\
-                  vmul.vv %[vPowReal],%[vReal],%[vReal];\
-                  vsra.vx %[vPowReal], %[vPowReal], %[mulShfit];\
-                  vmul.vv %[vPowImag],%[vImag],%[vImag];\
-                  vsra.vx %[vPowImag], %[vPowImag], %[mulShfit];\
-                  vadd.vv %[vAPow], %[vPowImag], %[vPowReal];\
-                  vle32.v %[vB], (%[a1Addr]);\
-                  vsrl.vx %[vImag], %[vB], %[num16];\
-                  vsll.vx %[vRealShfit], %[vB], %[num16];\
-                  vsrl.vx %[vReal], %[vRealShfit], %[num16];\
-                  vmul.vv %[vPowReal],%[vReal],%[vReal];\
-                  vmul.vv %[vPowImag],%[vImag],%[vImag];\
-                  vadd.vv %[vBPow], %[vPowImag], %[vPowReal];"
-                 :[vA]"+vr"(vA),[vB]"+vr"(vB),[vAPow]"=&vr"(vAPow),[vBPow]"=&vr"(vBPow),[vImag]"+vr"(vImag),[vReal]"+vr"(vReal),[vRealShfit]"+vr"(vRealShfit),[vPowImag]"+vr"(vPowImag),[vPowReal]"+vr"(vPowReal)
-                 :[a0Addr]"r"(a0Addr),[a1Addr]"r"(a1Addr),[num16]"r"(num16),[mulShfit]"r"(mulShfit));
-
-    vtypeE = M1 | E32;
-    asm volatile("vsetvl %[vl], %[avl], %[vtype]": [vl] "=r" (vl) : [avl] "r" (avl), [vtype] "r" (vtypeE));                                 
-    asm volatile("vsra.vv %[vAPowShif], %[vAPow], %[vShift];\
-                  vsra.vv %[vBPowShift], %[vBPow], %[vShift];\
-                  vadd.vv %[vPowSum], %[vAPowShif], %[vBPowShift];\
-                  vsra.vx %[vPowSumShift], %[vPowSum], %[accShfit];\
-                  vredsum.vs %[vSum], %[vPowSumShift], %[vAcc];"
-                 :[vSum]"=vr"(vSum),[vAPowShif]"+&vr"(vAPowShif),[vBPowShift]"+&vr"(vBPowShift),[vPowSum]"+&vr"(vPowSum),[vPowSumShift]"+&vr"(vPowSumShift)
-                 :[vShift]"vr"(vShift), [vAcc]"vr"(vAcc),[vAPow]"vr"(vAPow),[vBPow]"vr"(vBPow),[accShfit]"r"(accShfit));   
-                 
-  int32_t  volatile result;
-  asm volatile("vmv.x.s  %[result], %[vSum];"
-               :[result]"=r"(result)
-               :[vSum]"vr"(vSum));                                 
     return result;
 }
